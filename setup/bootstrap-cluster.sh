2f056c32d63c8c0d726496a65bbf131004bb839e
Alexander Untch <alexander@untch.com>
2021-07-07 13:56:38 +0200

    Add bootstrap-cluster.sh

---
+++ setup/bootstrap-cluster.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+echo Installing Server 1
+k3sup install \
+    --ip $K3S_CLUSTER_IP_M01 \
+    --user $K3S_CLUSTER_USER \
+    --cluster \
+    --merge --local-path $HOME/.kube/config-files/$K3S_CLUSTER_KUBECONFIG_NAME \
+    --k3s-extra-args "--disable traefik --disable servicelb --disable local-storage" \
+    --context $K3S_CLUSTER_CONTEXT \
+    --k3s-version $K3S_CLUSTER_VERSION
+
+echo Installing Server 2
+k3sup join \
+    --ip $K3S_CLUSTER_IP_M02 \
+    --user $K3S_CLUSTER_USER \
+    --server-ip $K3S_CLUSTER_IP_M01 \
+    --server-user $K3S_CLUSTER_USER \
+    --server \
+    --k3s-extra-args "--disable traefik --disable servicelb --disable local-storage" \
+    --k3s-version $K3S_CLUSTER_VERSION
+
+echo Installing Server 3
+k3sup join \
+    --ip $K3S_CLUSTER_IP_M03 \
+    --user $K3S_CLUSTER_USER \
+    --server-ip $K3S_CLUSTER_IP_M01 \
+    --server-user $K3S_CLUSTER_USER \
+    --server \
+    --k3s-extra-args "--disable traefik --disable servicelb --disable local-storage" \
+    --k3s-version $K3S_CLUSTER_VERSION
+
+echo Installing Worker 1
+k3sup join \
+    --ip $K3S_CLUSTER_IP_W01 \
+    --user $K3S_CLUSTER_USER \
+    --server-ip $K3S_CLUSTER_IP_M01 \
+    --k3s-version $K3S_CLUSTER_VERSION
+
+# echo Installing Worker 2
+
+# echo Installing Worker 3
+
+echo Installing Worker 4
+k3sup join \
+    --ip $K3S_CLUSTER_IP_W04 \
+    --user $K3S_CLUSTER_USER \
+    --server-ip $K3S_CLUSTER_IP_M01 \
+    --k3s-version $K3S_CLUSTER_VERSION
+
+# Install 'kubernetes-dashboard'
+echo Installing kubernetes-dashboard
+arkade install kubernetes-dashboard
+
+# Create 'flux-system' namespace
+echo Creating flux-system namespace
+kubectl create namespace flux-system
+
+# Create 
+
+# Add the Flux GPG key in-order for Flux to decrypt SOPS secrets
+# TODO: place encrypted sops-gpg.yaml in the repository
+gpg --export-secret-keys --armor "${FLUX_KEY_FP_TESTING}" |
+kubectl create secret generic sops-gpg \
+    --namespace=flux-system \
+    --from-file=sops.asc=/dev/stdin
