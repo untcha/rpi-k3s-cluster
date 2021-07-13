a0d52924f25c8d9a02eb2080cc34296387a32f6c
Alexander Untch <alexander@untch.com>
2021-07-13 19:13:26 +0200

    Updated bootstrap script

--- setup/bootstrap-cluster.sh
+++ setup/bootstrap-cluster.sh
@@ -58,19 +58,21 @@ k3sup join \
 #     --server-ip $K3S_CLUSTER_IP_M01 \
 #     --k3s-version $K3S_CLUSTER_VERSION
 
+source ~/.zshrc
+
 # Install 'kubernetes-dashboard'
-echo Installing kubernetes-dashboard
-arkade install kubernetes-dashboard
+#echo Installing kubernetes-dashboard
+#arkade install kubernetes-dashboard
 
 # Create 'flux-system' namespace
-echo Creating flux-system namespace
-kubectl create namespace flux-system
+#echo Creating flux-system namespace
+#kubectl create namespace flux-system
 
 # Create 
 
 # Add the Flux GPG key in-order for Flux to decrypt SOPS secrets
 # TODO: place encrypted sops-gpg.yaml in the repository
-gpg --export-secret-keys --armor "${FLUX_KEY_FP_TESTING}" |
-kubectl create secret generic sops-gpg \
-    --namespace=flux-system \
-    --from-file=sops.asc=/dev/stdin
+# gpg --export-secret-keys --armor "${FLUX_KEY_FP_TESTING}" |
+# kubectl create secret generic sops-gpg \
+#     --namespace=flux-system \
+#     --from-file=sops.asc=/dev/stdin
