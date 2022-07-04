802fb4762606063772d3bbaedba5fa3417adfd7a
Alexander Untch <alexander@untch.com>
2022-07-04 09:29:35 +0200

    Update digitalocean-dns-updater.sh / clean-up

--- cluster/apps/dns/do-dns-updater/digitalocean-dns-updater.sh
+++ cluster/apps/dns/do-dns-updater/digitalocean-dns-updater.sh
@@ -2,10 +2,8 @@
 
 #################### CHANGE THE FOLLOWING VARIABLES ####################
 TOKEN="${SECRET_DIGITALOCEAN_API_TOKEN}"
-DOMAIN="${SECRET_DIGITALOCEAN_DOMAIN_01}"
-RECORD_ID="${SECRET_DIGITALOCEAN_RECORD_ID_DOMAIN_01}"
-DOMAIN2="${SECRET_DIGITALOCEAN_DOMAIN_02}"
-RECORD_ID2="${SECRET_DIGITALOCEAN_RECORD_ID_DOMAIN_02}"
+DOMAIN="${SECRET_DOMAIN}"
+RECORD_ID="${SECRET_DIGITALOCEAN_RECORD_ID}"
 LOG_FILE="/data/digital-ocean-dns-updater-log.txt"
 ########################################################################
 
@@ -18,7 +16,6 @@ else
     echo "IP has changed: $CURRENT_IPV4"
     echo "$(date),$CURRENT_IPV4" >> "$LOG_FILE"
     curl -X PUT -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" -d '{"data":"'"$CURRENT_IPV4"'"}' "https://api.digitalocean.com/v2/domains/$DOMAIN/records/$RECORD_ID"
-    curl -X PUT -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" -d '{"data":"'"$CURRENT_IPV4"'"}' "https://api.digitalocean.com/v2/domains/$DOMAIN2/records/$RECORD_ID2"
 fi
 
 # https://salvatorelab.com/2020/10/how-to-point-a-domain-to-your-dynamic-home-ip-address/
